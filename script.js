5d8cab91566ed80bec5aad8ef7d91ac2cb84a84a
wkwyatt <wkwyatt@gmail.com>
2015-10-21 08:14:33 -0400

    Got the total to work and added easter egg
    added sixty-nine easter egg

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function  doIt(theExpression) {
 		timer = setInterval(function() {
 			var now = new Date,
 			deltaT = now - lastFrame;
-			element.style.left = (left += 15 * deltaT / 16) + "px";
+			element.style.left = (left += 10 * deltaT / 16) + "px";
 			lastFrame = now;
 			// clear the time and stop animation
 			if(left > 3000) {
@@ -30,9 +30,29 @@ function  doIt(theExpression) {
 			}
 		}, 16);
 	
-		
+	}
+
+	if(total == 69) {
+		// var sound = new Audio("spaceship.m4a");
+		// sound.play();
+		var element = document.getElementById('sixty-nine'),
+			left = -500,
+			lastFrame = +new Date,
+			timer;
+
+		// Move element right 600 px
+		timer = setInterval(function() {
+			var now = new Date,
+			deltaT = now - lastFrame;
+			element.style.left = (left += 5 * deltaT / 16) + "px";
+			lastFrame = now;
+			// clear the time and stop animation
+			if(left > 3000) {
+				clearInterval(timer);
+			}
+		}, 16);	
 	}	
 
-	total = eval(theExpression);
-	Calc.total.value = total;
+	// total = eval(theExpression);
+	Calc.Input.value = total;
 }
